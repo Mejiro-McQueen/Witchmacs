fcb4d30d121ac1f3ddc7e4e26b9b0e8fab8e251d
Adrian Vazquez <Adrian.Vazquez@jpl.nasa.gov>
2024-07-10 08:32:44 -0700

    Use Guix

--- init.el
+++ init.el
@@ -1,35 +1,9 @@
-;; Make emacs startup faster
-(setq gc-cons-threshold 402653184
-      gc-cons-percentage 0.6)
- 
-(defvar startup/file-name-handler-alist file-name-handler-alist)
-(setq file-name-handler-alist nil)
- 
-(defun startup/revert-file-name-handler-alist ()
-  (setq file-name-handler-alist startup/file-name-handler-alist))
- 
-(defun startup/reset-gc ()
-  (setq gc-cons-threshold 16777216
-    gc-cons-percentage 0.1))
- 
-(add-hook 'emacs-startup-hook 'startup/revert-file-name-handler-alist)
-(add-hook 'emacs-startup-hook 'startup/reset-gc)
-;;
-
-;; Initialize melpa repo
-(require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives
-        '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-
-;; Initialize use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; Load GUIX installed packages
+(let ((default-directory  "~/.guix-profile/share/emacs/site-lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
 
 ;; Load Witchmacs theme
 (load-theme 'Witchmacs t)
 
-;; Load config.org for init.el configuration
+;; Load Config
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
